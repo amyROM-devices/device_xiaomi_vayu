56a1244cac8bb2e630108b3617f1b6c3ddad3b92
Amy Grace <amy@blisslabs.org>
2021-08-11 15:14:48 +0300

    vayu: amy-ify / Replaced LineageOS stuff with amyROM ones. / Tracking our SM8150 common tree and hardware blobs based on LineageOS ones. / Tracking mochi kernel tree from Jebaitedneko. / Written custom vendorsetup.sh, which will clone vendor tree from The Muppets GitLab. / Added maintainer overlay.
    Co-authored-by: Lambada10 <62511588+Lambada10@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 amyROM
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/amy_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    amy_vayu-user \
+    amy_vayu-userdebug \
+    amy_vayu-eng
