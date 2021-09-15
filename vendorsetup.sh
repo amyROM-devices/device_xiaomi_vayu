5636af358fc595286e5bba3b559f755dd6122a38
Wind/owZ <windowz414@gnuweeb.org>
2021-09-15 23:24:28 +0300

    vayu: Move tracking of vendor tree to dependencies
    * https://github.com/amyROM/android/commits/f134f8c8577309c96734cd643b1722b333de0a9a
    
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-shift
-echo "//     owZ' vendorsetup.sh     //"
-echo "// Specialized for vayu! (≧▽≦) //"
-echo ""
-echo "Please hang on. Cloning vendor tree..."
-echo ""
-git clone https://gitlab.com/the-muppets/proprietary_vendor_xiaomi.git -b lineage-18.1 vendor/xiaomi > /dev/null && echo "Cloning done, now you're all ready to build! (✿^‿^)" || echo "Cloning failed. Please try the last command again..."
